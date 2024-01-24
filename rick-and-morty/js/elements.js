ddf909300e4cad7f53eff6f33e4a2f1b29a523c6
n.balazs <balazsnagy220@gmail.com>
2024-01-24 17:54:11 +0100

    Typo correction

---
+++ rick-and-morty/js/elements.js
@@ -0,0 +1,48 @@
+// Gomb kiválasztása az azonosítója alapján
+var createNewElementButton = document.getElementById("createNewElement");
+
+
+
+
+// div elem kiválasztása az azonosítója alapján
+var existingElement = document.getElementById("existingElement");
+
+
+
+
+// Gomb eseménykezelő hozzáadása
+createNewElementButton.addEventListener("click", function() {
+    
+    var newDiv = document.createElement("div");
+
+
+    //tartalom beállítás
+    newDiv.innerHTML = "New element"; 
+
+    
+    newDiv.style.backgroundColor = "lightblue";
+    newDiv.style.padding = "10px";
+    newDiv.style.marginLeft = "40%";
+    newDiv.style.width="20%";
+    newDiv.style.backgroundColor = "red";
+
+   
+    document.body.appendChild(newDiv);
+});
+
+
+var modifyElementButton = document.getElementById("modifyElement");
+
+var myDiv = document.getElementById("myDiv");
+
+
+
+
+// Gomb eseménykezelő hozzáadása
+modifyElementButton.addEventListener("click", function() {
+
+
+    // Meglévő div elem tartalmának módosítása
+    myDiv.style.color = "red"; // Beállítjuk a színét pirosra
+    myDiv.innerHTML = "this elem modified"; // Módosítjuk a tartalmát
+});
